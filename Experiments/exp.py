239956050ae91d4329d0e6ce97afe257d387e921
Yousif <95356845+Yousif-Al-Khairi@users.noreply.github.com>
2024-09-13 11:14:13 +0200

    reorg

---
+++ Experiments/exp.py
@@ -0,0 +1,12 @@
+#Imports
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+#from graphviz import Digraph
+#import torch
+#from sklearn.datasets import make_moons
+from micrograd.nn import *
+from micrograd.engine import Value
+
+print(Value(2))
